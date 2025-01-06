2044547be15fc5f91049f46ff3b856885d6910a0
Idris <macbookpro@Idriss-MBP.lan>
2025-01-07 00:24:00 +0100

    All needed files and Api resources are created

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('------ FilmSamlaren js loader ------');
+
+const apiKey = '';
+const apiUrl = '';
+
+
+
+let favoriteMovies = JSON.parse(localStorage.getItem('Favorites')) || [];
+let showingPopular = false;
