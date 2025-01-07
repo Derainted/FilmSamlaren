3f0277732db6ebe7e7bdf2f6fb64fa56881d25ac
Idris <macbookpro@Idriss-MBP.lan>
2025-01-07 08:18:11 +0100

    completed but not merged yet

--- js/script.js
+++ js/script.js
@@ -1,148 +1,163 @@
 const apiKey = '6f61570b5e439d0714c98202884ac5ff';
 const baseUrl = 'https://api.themoviedb.org/3/';
 
+const inputSearch = document.getElementById('search');
+const favoriteBtn = document.getElementById('favorite-toggle');
+const popularBtn = document.getElementById('show-popular');
+const movieList = document.getElementById('movie-list');
+const loadingIndicator = document.getElementById('loading');
+
+
 
 let favoriteMovies = JSON.parse(localStorage.getItem('favorites')) || [];
 let showingPopular = false;
 
 // Search query fetch .. 
 async function fetchMovies(query = '') {
-  const url = `${baseUrl}search/movie?api_key=${apiKey}&query=${query}&language=en-US&page=1&include_adult=false`;
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    displayMovies(data.results);
-  } catch (error) {
-    console.error('Error fetching movies:', error);
-  }
+    const url = `${baseUrl}search/movie?api_key=${apiKey}&query=${query}&language=en-US&page=1&include_adult=false`;
+    try {
+        // Displays loading indicator
+        loadingIndicator.style.display = 'block';
+
+        const response = await fetch(url);
+        const data = await response.json();
+        displayMovies(data.results);
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+    } finally {
+        loadingIndicator.style.display = 'none';
+    }
 }
 
 // Fetch popular movies
 async function fetchPopularMovies() {
-  const url = `${baseUrl}movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    displayMovies(data.results);
-  } catch (error) {
-    console.error('Error fetching popular movies:', error);
-  }
+    const url = `${baseUrl}movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+    try {
+        // Displays loading indicator
+        loadingIndicator.style.display = 'block';
+
+        const response = await fetch(url);
+        const data = await response.json();
+        displayMovies(data.results);
+    } catch (error) {
+        console.error('Error fetching popular movies:', error);
+    } finally {
+        loadingIndicator.style.display = 'none';
+    }
 }
 
 // Display movies for the DOM 
 function displayMovies(movies) {
-  const movieList = document.getElementById('movie-list');
-  movieList.innerHTML = '';
+    movieList.innerHTML = '';
 
-  if (movies.length === 0) {
-    movieList.innerHTML = '<p>No movies found</p>';
-    return;
-  }
+    if (movies.length === 0) {
+        movieList.innerHTML = '<p>No movies found</p>';
+        return;
+    }
 
-  movies.forEach(movie => {
-    const movieItem = document.createElement('div');
-    movieItem.classList.add('movie-item');
+    movies.forEach(movie => {
+        const movieItem = document.createElement('div');
+        movieItem.classList.add('movie-item');
 
-    const image = document.createElement('img');
-    image.src = movie.backdrop_path 
-    ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` 
-    : "https://placehold.co/500x281";
+        const image = document.createElement('img');
+        image.src = movie.backdrop_path
+            ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+            : "https://placehold.co/500x281";
 
-    const title = document.createElement('h3');
-    title.textContent = movie.title;
+        const title = document.createElement('h3');
+        title.textContent = movie.title;
 
-    const buttonDetail = document.createElement('button');
-    buttonDetail.innerText = 'Details';
-    buttonDetail.onclick = () => showMovieDetails(movie.id);
+        const buttonDetail = document.createElement('button');
+        buttonDetail.innerText = 'Details';
+        buttonDetail.onclick = () => showMovieDetails(movie.id);
 
-    // Create the favorite button and set its text based on whether the movie is a favorite
-    const buttonFavorite = document.createElement('button');
-    buttonFavorite.innerText = isFavorite(movie.id) ? 'Remove from Favorites' : 'Add to Favorites';
-    buttonFavorite.setAttribute('data-movie-id', movie.id);
-    buttonFavorite.onclick = () => toggleFavorite(movie.id);
+        // Create the favorite button and set its text based on whether the movie is a favorite
+        const buttonFavorite = document.createElement('button');
+        buttonFavorite.innerText = isFavorite(movie.id) ? 'Remove from Favorites' : 'Add to Favorites';
+        buttonFavorite.setAttribute('data-movie-id', movie.id);
+        buttonFavorite.onclick = () => toggleFavorite(movie.id);
 
-    movieItem.appendChild(image);
-    movieItem.appendChild(title);
-    movieItem.appendChild(buttonDetail);
-    movieItem.appendChild(buttonFavorite);
-    movieList.appendChild(movieItem);  
-    
-  });
+        movieItem.appendChild(image);
+        movieItem.appendChild(title);
+        movieItem.appendChild(buttonDetail);
+        movieItem.appendChild(buttonFavorite);
+        movieList.appendChild(movieItem);
+    });
 }
 
 // Show movie details
 function showMovieDetails(movieId) {
-  const url = `${baseUrl}movie/${movieId}?api_key=${apiKey}&language=en-US`;
-  fetch(url)
-    .then(response => response.json())
-    .then(movie => {
-      // Elements for popup window/modal window
-      const modal = document.getElementById('movie-modal');
-      const modalTitle = document.getElementById('modal-title');
-      const modalImage = document.getElementById('modal-image');
-      const modalGenre = document.getElementById('modal-genre');
-      const modalDescription = document.getElementById('modal-description');
-      const closeModal = document.getElementById('close-modal');
-      
-      // Fill modal with movie details
-      modalTitle.textContent = `${movie.title} (${movie.release_date.split('-')[0]})`;
-      modalImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      modalGenre.innerHTML = `<strong>Genre:</strong> ${movie.genres.map(genre => genre.name).join(', ')}`;
-      modalDescription.innerHTML = `<strong>Description:</strong> ${movie.overview}`;
+    const url = `${baseUrl}movie/${movieId}?api_key=${apiKey}&language=en-US`;
+    fetch(url)
+        .then(response => response.json())
+        .then(movie => {
+            // Elements for popup window/modal window
+            const modal = document.getElementById('movie-modal');
+            const modalTitle = document.getElementById('modal-title');
+            const modalImage = document.getElementById('modal-image');
+            const modalGenre = document.getElementById('modal-genre');
+            const modalDescription = document.getElementById('modal-description');
+            const closeModal = document.getElementById('close-modal');
 
-      modal.style.display = 'block';
+            // Fill modal with movie details
+            modalTitle.textContent = `${movie.title} (${movie.release_date.split('-')[0]})`;
+            modalImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            modalGenre.innerHTML = `<strong>Genre:</strong> ${movie.genres.map(genre => genre.name).join(', ')}`;
+            modalDescription.innerHTML = `<strong>Description:</strong> ${movie.overview}`;
 
-      // Close the modal when the user clicks the close button
-      closeModal.onclick = function() {
-          modal.style.display = 'none';
-      }
+            modal.style.display = 'block';
 
-      // Close the modal if the user clicks outside the modal content
-      window.onclick = function(event) {
-          if (event.target === modal) {
-              modal.style.display = 'none';
-          }
-      }
-  })
-  .catch(error => console.error('Error fetching movie details', error));
+            // Close the modal when the user clicks the close button
+            closeModal.onclick = function () {
+                modal.style.display = 'none';
+            }
+
+            // Close the modal if the user clicks outside the modal content
+            window.onclick = function (event) {
+                if (event.target === modal) {
+                    modal.style.display = 'none';
+                }
+            }
+        })
+        .catch(error => console.error('Error fetching movie details', error));
 }
 
 
 // Toggle favorite movies
 function toggleFavorite(movieId) {
-  const button = document.querySelector(`button[data-movie-id="${movieId}"]`);
-  if (isFavorite(movieId)) {
-    favoriteMovies = favoriteMovies.filter(id => id !== movieId);
-    button.textContent = 'Add to Favorites';
-  } else {
-    favoriteMovies.push(movieId);
-    button.textContent = 'Remove from Favorites';
-  }
-  localStorage.setItem('favorites', JSON.stringify(favoriteMovies));
+    const button = document.querySelector(`button[data-movie-id="${movieId}"]`);
+    if (isFavorite(movieId)) {
+        favoriteMovies = favoriteMovies.filter(id => id !== movieId);
+        button.textContent = 'Add to Favorites';
+    } else {
+        favoriteMovies.push(movieId);
+        button.textContent = 'Remove from Favorites';
+    }
+    localStorage.setItem('favorites', JSON.stringify(favoriteMovies));
 }
 
 // Check if a movie is a favorite
 function isFavorite(movieId) {
-  return favoriteMovies.includes(movieId);
+    return favoriteMovies.includes(movieId);
 }
 
 // Show favorites
 function showFavorites() {
-  const movieList = document.getElementById('movie-list');
-  movieList.innerHTML = ''; // Clear existing movie list
+    const movieList = document.getElementById('movie-list');
+    movieList.innerHTML = ''; // Clear existing movie list
 
-  // If no favorite movies, show a message
-  if (favoriteMovies.length === 0) {
-    movieList.innerHTML = '<p>No favorites found.</p>';
-    return;
-  }
+    // If no favorite movies, show a message
+    if (favoriteMovies.length === 0) {
+        movieList.innerHTML = '<p>No favorites found.</p>';
+        return;
+    }
 
-  // Fetch movie details for each favorite
-  const favoriteMoviesDetails = favoriteMovies.map(id => {
-    const url = `${baseUrl}movie/${id}?api_key=${apiKey}&language=en-US`;
-    return fetch(url)
-      .then(response => response.json())
-      .then(movie => `
+    // Fetch movie details for each favorite
+    const favoriteMoviesDetails = favoriteMovies.map(id => {
+        const url = `${baseUrl}movie/${id}?api_key=${apiKey}&language=en-US`;
+        return fetch(url)
+            .then(response => response.json())
+            .then(movie => `
         <div class="movie-item">
           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
           <h3>${movie.title}</h3>
@@ -152,40 +167,38 @@ function showFavorites() {
           </button>
         </div>
       `);
-  });
+    });
 
-  // Render the updated favorites list
-  Promise.all(favoriteMoviesDetails)
-    .then(favoriteHtml => {
-      movieList.innerHTML = favoriteHtml.join('');
-    })
-    .catch(error => console.error('Error fetching favorite movies:', error));
+    // Render the updated favorites list
+    Promise.all(favoriteMoviesDetails)
+        .then(favoriteHtml => {
+            movieList.innerHTML = favoriteHtml.join('');
+        })
+        .catch(error => console.error('Error fetching favorite movies:', error));
 }
 
-  
 // Event listeners for the index page
-if (document.getElementById('search')) {
-  document.getElementById('search').addEventListener('input', (event) => {
-    const query = event.target.value;
-    if (!showingPopular) fetchMovies(query);
-  });
+if (inputSearch) {
+    inputSearch.addEventListener('input', (event) => {
+        const query = event.target.value;
+        showingPopular = false;  // Resetting the flag
+        if (query) fetchMovies(query);
+    });
 }
+
 
 // Event listeners for the fav toggle
-if (document.getElementById('favorite-toggle')) {
-  document.getElementById('favorite-toggle').addEventListener('click', showFavorites);
-}
+favoriteBtn.addEventListener('click', showFavorites);
 
 // Event listeners for the show poppular page
-if (document.getElementById('show-popular')) {
-  document.getElementById('show-popular').addEventListener('click', () => {
+
+popularBtn.addEventListener('click', () => {
     window.location.href = '/html/popularMovies.html'; // Navigates to popularmovies.html
-  });
-}
+});
 
 // Fetch popular movies on the popularMOVIES.html page
-if (document.body.contains(document.getElementById('movie-list')) && window.location.pathname.includes('/html/popularMovies.html')) {
-  fetchPopularMovies();
+if (document.body.contains(movieList) && window.location.pathname.includes('/html/popularMovies.html')) {
+    fetchPopularMovies();
 }
 
-fetchMovies('new');
+fetchMovies('scary');
